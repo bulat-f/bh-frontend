4d8b26a485a465f70ef5c355b6b81ec94091aefa
Bulat F <write2bulat@gmail.com>
2018-08-30 23:13:48 +0300

    Implement clickable tasks list

--- src/app/tracker/tracker.component.ts
+++ src/app/tracker/tracker.component.ts
@@ -9,6 +9,29 @@ export class TrackerComponent implements OnInit {
   started = false
   seconds = 1000
   interval = null
+  tasks = [
+    {
+      id: 1,
+      title: 'Task 1',
+      duration: 100
+    },
+    {
+      id: 2,
+      title: 'Task 2',
+      duration: 100
+    },
+    {
+      id: 3,
+      title: 'Task 3',
+      duration: 100
+    },
+    {
+      id: 4,
+      title: 'Task 4',
+      duration: 100
+    }
+  ]
+  activeTaskId = null
 
   constructor() { }
 
@@ -22,5 +45,9 @@ export class TrackerComponent implements OnInit {
     } else {
       clearInterval(this.interval)
     }
+  }
+
+  onSelect(id) {
+    this.activeTaskId = id
   }
 }
